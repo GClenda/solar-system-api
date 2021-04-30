6d06bae257036007e283620de81f4211a27af630
Glenda <chicag9313@gmail.com>
2021-04-30 15:02:57 -0700

    We add endpoint for client to add planets data

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
 from flask import Blueprint
+from app import db
+from app.models.planet import Planet
+from flask import request
+from flask import request, make_response
+from flask import jsonify
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+@planets_bp.route("", methods=["POST"], strict_slashes=False)
+def planets():
+
+    request_body = request.get_json()
+    new_planet = Planet(name = request_body["name"], 
+                                description = request_body["description"],
+                                diameter = request_body["diameter"])
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+    
